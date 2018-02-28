4aebb132fbe5fd154a4eafc83476ae29518179eb
frikha <frikha@rhrk.uni-kl.de>
2018-02-28 10:07:30 +0100

    Html exp

---
+++ crdt-visualizer/src/components/RemoveUpdate.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import {Rect, Text, Group} from 'react-konva';
+
+
+interface States {
+}
+
+interface Props {
+    x: number;
+    y: number;
+    visible: boolean;
+}
+
+class RemoveUpdate extends React.Component<Props, States> {
+    constructor(props: Props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <Group visible={this.props.visible}>
+                <Rect
+                    x={this.props.x} y={this.props.y}
+                    stroke={'#555'}
+                    strokeWidth={2}
+                    fill={'#ddd06d'}
+                    width={150}
+                    height={220}
+                    shadowColor={'black'}
+                    shadowBlur={10}
+                    shadowOpacity={0.2}
+                    cornerRadius={10}
+
+                />
+                <Text text={'Move hier to trash the update'} fill={'#555'}
+                      x={this.props.x} y={this.props.y} fontSize={13} width={150} padding={20} align={'center'}
+                      fontFamily={'Calibri'}/>
+            </Group>);
+    }
+
+
+}
+
+
+export default RemoveUpdate;
