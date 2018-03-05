a52cff992917d5ca5eb323bedb2867013300a5a0
Rasha_Abu_Qasem <Rasha.aq@gmail.com>
2018-03-05 23:38:43 +0100

    set bug fix

--- crdt-visualizer/src/classes/Addwinn_Set.tsx
+++ crdt-visualizer/src/classes/Addwinn_Set.tsx
@@ -5,50 +5,77 @@ import {CRDT_type} from './CRDT_type';
     tag:number;
 }
 export type  AddWinsSet_state = AddWinnsSetStateType[];
+class set_type{
+	
+	element:string;
+	tag:number;
+	constructor(e:string, t:number){
+		this.element = e;
+		this.tag= t;
+	}
+	
+}
 
-
-export type  AddWinsSet_Downstream =  {op: "add", added: AddWinnsSetStateType}| {op: "remove", removed_elements:AddWinsSet_state};
+export type  AddWinsSet_Downstream =  {op: "add", added: AddWinnsSetStateType}| {op: "remove", removed:AddWinnsSetStateType};
 
 export class Addwinn_Set extends CRDT_type {
 
 //---------- Properties---------------------    
 
-    state: AddWinsSet_state;
+    //state: AddWinsSet_state;
     last_tag:number;    
     AddWinsSetOperation :	{op: "add"; element:number}  | {op: "remove"; element: number};
-
+	delete_tage: number;
+	downstream_counter:number;
+	 state: set_type []=[];
 //----------Metods--------------------------
 
 //////////CONSTRUCTOR///////////////////////
     constructor() {
         super();
         this.state = [];
-        this.last_tag=0;
+		this.last_tag=1;
+		this.delete_tage=1;
+		this.downstream_counter=1;
     }
 
     init(){
-        this.state = [];
-        this.last_tag=0;
-    }
+		this.state = [];
+		//this.test=[];
+		this.last_tag=1;
+		this.delete_tage=1;
+		this.downstream_counter=1;
+	}
 
     new_downstream(downstream_effect: AddWinsSet_Downstream){
+		
+		var mytest: set_type []=[];
         if (downstream_effect.op == "add") {
-			 this.state.push(downstream_effect.added);
-		} else {
-			let newState: AddWinsSet_state = [];
-			for (let e1 of this.state) {
-				let removed = false;
-				for (let e2 of downstream_effect.removed_elements) {
-					if ((e1.element === e2.element)&&(e1.tag==e2.tag)) {
-						removed = true;
-						break;
-					}
-				}
-				if (!removed) {
-					newState.push(e1);
-				}
-			}
 			
+			 var se: set_type;
+			 var n : number= this.downstream_counter;
+			 var st: string= downstream_effect.added.element;
+			 se= new set_type(st,n);
+
+			 mytest.push(se);
+			 this.state.push(se);
+			 this.downstream_counter=this.downstream_counter+1;
+			
+		
+		} else {
+			let newState: set_type []=[];			
+			for (var i = 0; i < this.state.length; i++) {
+				
+				let removed = false;
+					if (this.state[i].element == downstream_effect.removed.element) {
+							if ((this.state[i].tag<downstream_effect.removed.tag)||(this.state[i].tag==downstream_effect.removed.tag)){
+							removed = true;
+						}
+					}
+				if (!removed) {
+					newState.push(this.state[i]);
+				}
+			} 
 			this.state= newState;
 		}
        
@@ -56,19 +83,16 @@ export class Addwinn_Set extends CRDT_type {
 
 
     new_at_source(operation_name: string, elem: string): AddWinsSet_Downstream {
-		
+		var c: AddWinsSet_Downstream;
         if (operation_name == "remove") {
-			let R: AddWinsSet_state = []
-			for (let e of this.state) {
-				if (e.element === elem) {
-					R.concat(e)
-				}
-			} 			
-			return {op: "remove", removed_elements: R};
+			this.delete_tage=this.last_tag;
+			c= {op: "remove", removed: {element: elem, tag: this.delete_tage}};
+			return c;
 		} else {
+			let tag = this.last_tag;
+			c= {op: "add", added: {element: elem, tag: tag}};
 			this.last_tag++;
-            let tag = this.last_tag;
-			return {op: "add", added: {element: elem, tag: tag}};
+			return c;
         }        
        
 	}
@@ -76,7 +100,7 @@ export class Addwinn_Set extends CRDT_type {
 	display(): string {
 		let result : string = '{ ';
 		for (var i=0 ; i< this.state.length;i++ ){
-			result += '( ' + this.state[i].element + ' )';
+			result += '( ' + this.state[i].element + ', '+ this.state[i].tag +   ')';
 		}
 		result+=' }';
 		return result;
