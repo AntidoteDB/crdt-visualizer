f3e5964899e7e6ae1c5dc00b50d055d16a0d5122
dilan400388 <dlnshaminda@gmail.com>
2018-07-17 20:27:40 +0200

    -UI for Integer data type

---
+++ src/classes/crdts/integer.tsx
@@ -0,0 +1,51 @@
+import { CRDT_type, Operation } from '../CRDT_type';
+
+export type counter_downstream = number;
+export type counter_state = number;
+
+export class Integer implements CRDT_type<counter_downstream, counter_state> {
+
+
+    initialState(): number {
+        return 0;
+    }
+
+    downstream(operation: Operation, uid: string, state: number): number {
+        if (operation.name === "increment") {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    update(downstream: number, state: number): number {
+        return state + downstream;
+    }
+    value(state: number): string {
+        return state.toString();
+    }
+    defaultOperation(): string {
+        return "increment";
+    }
+    operationSuggestions(): string[] {
+        return ["increment", "decrement","set","reset"];
+    }
+    checkOperation(operation: Operation): string | null {
+        if (operation.name != "increment" && operation.name != "decrement" 
+            && operation.name != "set" && operation.name != "reset") {
+            return "Unsupported operation: " + operation.name;
+        }
+        if (operation.args.length != 0) {
+            return "No arguments expected";
+        }
+        return null;
+    }
+    stateToString(state: number): string {
+        return state.toString();
+    }
+    downstreamToString(downstream: number): string {
+        return downstream.toString();
+    }
+
+
+}
+
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import {Set_aw} from './classes/crdts/set_aw'
 import {Counter} from "./classes/crdts/counter";
-
+import {Integer} from "./classes/crdts/integer";
 
 
 ReactDOM.render(
@@ -16,5 +16,11 @@ ReactDOM.render(
 ReactDOM.render(
     <App crdt={new Set_aw()} />,
     document.getElementById('addWinSet') as HTMLElement,
+
+);
+
+ReactDOM.render(
+    <App crdt={new Integer()} />,
+    document.getElementById('gMaps') as HTMLElement,
 
 );
